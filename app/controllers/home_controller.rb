17f018597ab92d8adb43042a55823510c90df4d4
NIcoZF <nico-7@hotmail.fr>
2019-09-26 16:29:30 +0200

    replace rails ujs with jquery-rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,18 @@
 
 class HomeController < ApplicationController
   def index
-    results = Geocoder.search("Paris")
-    @stations = Station.all.where("free_bikes != 0").near(results.first.coordinates, 5).take(5)
+    if params[:place] != "" && !params[:place].nil?
+
+      @place = params[:place]
+      results = Geocoder.search(@place)
+      @stations = Station.all.where("free_bikes != 0").near(results.first.coordinates, 5).take(5)
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    else
+
+      @stations == nil
+    end
   end
 end
